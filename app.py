5565bdbd2b1c10c606c533a69bf0ee5fcc8ce2ec
abhishekmishramm1997 <abhishekmishramm1997@gmail.com>
2024-04-12 13:08:49 +0530

    Version 1.0
    Pushing initial changes

--- app.py
+++ app.py
@@ -9,6 +9,58 @@ from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
+import os
+
+
+os.environ['OPENAI_API_KEY'] = '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'
+
+
+
+# streamlit_app.py
+
+import hmac
+import streamlit as st
+
+
+def check_password():
+    """Returns `True` if the user had a correct password."""
+
+    def login_form():
+        """Form with widgets to collect user information"""
+        with st.form("Credentials"):
+            st.text_input("Username", key="username")
+            st.text_input("Password", type="password", key="password")
+            st.form_submit_button("Log in", on_click=password_entered)
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["username"] in st.secrets[
+            "passwords"
+        ] and hmac.compare_digest(
+            st.session_state["password"],
+            st.secrets.passwords[st.session_state["username"]],
+        ):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the username or password.
+            del st.session_state["username"]
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the username + password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show inputs for username + password.
+    login_form()
+    if "password_correct" in st.session_state:
+        st.error("😕 User not known or password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()
+
+
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -102,3 +154,7 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+# Main Streamlit app starts here
+st.write("Here goes your normal Streamlit app...")
+st.button("Click me")
